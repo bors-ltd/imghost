9014d6d215f429073782b942b9d3046ce36a2e5a
Hervé Cauwelier <github@bors-ltd.fr>
2014-11-10 12:44:34 +0100

    images: remove special case for hosting media

--- src/images/templatetags/images.py
+++ src/images/templatetags/images.py
@@ -7,5 +7,4 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def full_img_url(context, url):
     request = context['request']
-    clean_url = url.replace('/media/i/', '/i/')
-    return request.build_absolute_uri(clean_url)
+    return request.build_absolute_uri(url)
