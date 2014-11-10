7cf99fd38d5489fa5206286d756950564aeacdf5
Hervé Cauwelier <herve@cauwelier.net>
2014-11-10 17:18:51 +0100

    images: permission to add image
    split memes?

--- src/memes/urls.py
+++ src/memes/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns(
     'memes.views',
+    url(r'^create/$', 'create_meme', name='create_meme'),  # Before so 'create' does not match as a unique key
     url(r'^(?P<unique_key>\w+)/$', 'meme', name='meme'),
 )
--- src/memes/views.py
+++ src/memes/views.py
@@ -1,40 +1,54 @@
-import mimetypes
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, print_function, unicode_literals
 import json
-from django.shortcuts import get_object_or_404
+import mimetypes
+
+from django.contrib.auth.decorators import permission_required
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from django.views.decorators.http import require_POST, require_GET
+
 from annoying.decorators import render_to
 
 from images.models import Image
 from memes.forms import MemeUploadForm
 
 
+@require_GET
 @render_to('meme.html')
 def meme(request, unique_key):
     image = get_object_or_404(Image, unique_key=unique_key)
     mime = mimetypes.guess_type(image.image.url)[0]
 
-    if request.method == 'POST':
-        form = MemeUploadForm(data=request.POST)
-        if form.is_valid():
-            data = form.cleaned_data
-            img = Image.objects.create(
-                image=data['file'],
-                source='',
-                extension='png',
-                is_meme=True,
-                source_image=data['source_image']
-            )
-
-            json_data = json.dumps({
-                'redirect': img.get_absolute_url()
-            })
-        else:
-            json_data = json.dumps({
-                'errors': form.errors["__all__"],
-            })
-        return HttpResponse(json_data, content_type="application/x-javascript")
-
     return {
         'image': image,
         'mime': mime,
     }
+
+
+@require_POST
+@permission_required('images.add_image', raise_exception=True)
+@render_to("meme.html")
+def create_meme(request):
+    form = MemeUploadForm(data=request.POST)
+
+    if form.is_valid():
+        data = form.cleaned_data
+        img = Image.objects.create(
+            image=data['file'],
+            source='',
+            extension='png',
+            is_meme=True,
+            source_image=data['source_image']
+        )
+
+        json_data = json.dumps({
+            'redirect': img.get_absolute_url()
+        })
+
+    else:
+        json_data = json.dumps({
+            'errors': form.errors.get("__all__", ["Something went wrong. ‎(ﾉಥ益ಥ)ﾉ"]),
+        })
+
+    return HttpResponse(json_data, content_type="application/x-javascript")
