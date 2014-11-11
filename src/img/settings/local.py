66dc989c8e2f5cba03a096d85d2107d67c8fee75
Hervé Cauwelier <herve@cauwelier.net>
2014-11-11 14:15:57 +0100

    settings: dummy cache in dev

--- src/img/settings/local.py
+++ src/img/settings/local.py
@@ -21,3 +21,9 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.messages.context_processors.messages',
     'django.core.context_processors.request',
 )
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+    }
+}
