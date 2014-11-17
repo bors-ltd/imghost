ea58640244c8c6dcc0ebe0939ffe52f65530836f
Hervé Cauwelier <herve@cauwelier.net>
2014-11-17 21:08:20 +0100

    images: new all-in-one show_tags

--- src/images/templatetags/tags.py
+++ src/images/templatetags/tags.py
@@ -3,6 +3,9 @@ from __future__ import absolute_import, print_function, unicode_literals
 import itertools
 
 from django import template
+from django.db import models
+
+from images import models as images_models
 
 
 register = template.Library()
@@ -20,6 +23,19 @@ ALL_LABELS = (
 label_sequence = itertools.cycle(ALL_LABELS)
 
 
-@register.simple_tag()
-def next_label():
-    return label_sequence.next()
+@register.inclusion_tag("snippets/tags.html")
+def show_tags(tags=None):
+    if tags is None:
+        tags = images_models.Tag.objects.annotate(count=models.Count('tags'))
+    else:
+        tags = tags.all()
+
+    annotated_tags = []
+    for tag in tags:
+        tag.font_size = "%d%%" % (75 + 20 * getattr(tag, 'count', 0))
+        tag.css_class = label_sequence.next()
+        annotated_tags.append(tag)
+
+    return {
+        'tags': annotated_tags,
+    }
