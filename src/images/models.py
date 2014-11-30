182b4698d68b954e0cac6a690f60342cf0234dfd
Hervé Cauwelier <github@bors-ltd.fr>
2014-11-30 12:01:26 +0100

    images: obsolete perm, drop idea of third-party accounts

--- src/images/models.py
+++ src/images/models.py
@@ -79,7 +79,6 @@ class Image(models.Model):
         ordering = ('-created_on',)
         permissions = (
             ('add_meme', "Can add Meme"),
-            ('manage_tags', "Can manage Tags for an image"),
         )
 
     def __unicode__(self):
